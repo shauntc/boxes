a29542eed3a5e8858488dd81a90ed8d9578dcb1b
shauntc <shaun@shauntc.com>
2016-09-06 08:57:13 -0700

    Initial Commit

---
+++ boxes/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  boxes
+//
+//  Created by Shaun Campbell on 2016-09-06.
+//  Copyright © 2016 Shaun Campbell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
