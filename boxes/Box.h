1ec6cb2bb026139a336338f1ef8fcb9f54b0bc0a
shauntc <shaun@shauntc.com>
2016-09-06 09:52:03 -0700

    Initialize

---
+++ boxes/Box.h
@@ -0,0 +1,13 @@
+//
+//  Box.h
+//  boxes
+//
+//  Created by Shaun Campbell on 2016-09-06.
+//  Copyright © 2016 Shaun Campbell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Box : NSObject
+
+@end
---
+++ boxes/Box.m
@@ -0,0 +1,13 @@
+//
+//  Box.m
+//  boxes
+//
+//  Created by Shaun Campbell on 2016-09-06.
+//  Copyright © 2016 Shaun Campbell. All rights reserved.
+//
+
+#import "Box.h"
+
+@implementation Box
+
+@end
